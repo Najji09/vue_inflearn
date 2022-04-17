ff60b891c8beeb8972d81b57c36738ced4e6f8fb
Najji09 <100065117+Najji09@users.noreply.github.com>
2022-04-17 22:13:16 +0900

    watch

--- src/views/WatchVue.vue
+++ src/views/WatchVue.vue
@@ -1,17 +1,42 @@
 <template>
-  <div></div>
+  <div>
+    x:<input type="text" v-model.number="x" /> + y:<input
+      type="text"
+      v-model.number="y"
+    />
+    <br />
+    <button @click="addNum">Click to add</button>
+    <br />
+    result:<input type="text" v-model.number="z" />
+  </div>
 </template>
 <script>
 export default {
   name: 'watchVue',
   components: {},
   data() {
-    return {};
+    return {
+      x: 0,
+      y: 0,
+      z: 0,
+    };
   },
   setup() {},
   created() {},
   mounted() {},
   unmouted() {},
-  methods: {},
+  methods: {
+    addNum() {
+      this.z = this.x + this.y;
+    },
+  },
+  watch: {
+    x() {
+      this.z = this.x + this.y;
+    },
+    y() {
+      this.z = this.x + this.y;
+    },
+  },
 };
 </script>

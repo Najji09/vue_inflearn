c71ccc61b4ce9286bc0e85312bee9ac9c810ad9f
Najji09 <100065117+Najji09@users.noreply.github.com>
2022-04-17 21:27:04 +0900

    computed

--- src/views/ComputedVue.vue
+++ src/views/ComputedVue.vue
@@ -1,17 +1,34 @@
 <template>
-  <div></div>
+  <div>
+    <input type="text" v-model="firstName" />
+    <input type="text" v-model="lastName" />
+    <h1>Hello {{ fullNameComputed }}</h1>
+    <h1>Hello {{ fullNameMethod() }}</h1>
+  </div>
 </template>
 <script>
 export default {
   name: 'computedVue',
   components: {},
   data() {
-    return {};
+    return {
+      firstName: 'Jane',
+      lastName: 'Johnson',
+    };
   },
   setup() {},
   created() {},
   mounted() {},
   unmouted() {},
-  methods: {},
+  methods: {
+    fullNameMethod() {
+      return this.firstName + ' ' + this.lastName;
+    },
+  },
+  computed: {
+    fullNameComputed() {
+      return this.firstName + ' ' + this.lastName;
+    },
+  },
 };
 </script>
